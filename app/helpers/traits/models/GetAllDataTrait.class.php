c768abf8b147f39d3a605df07b9701486ae91658
Thomas DUDOUX <thomas.dudoux@gmail.com>
2017-04-07 13:23:20 +0200

    Added OrderBy in Query Builder and Front/Models/Content Done

--- app/helpers/traits/models/GetAllDataTrait.class.php
+++ app/helpers/traits/models/GetAllDataTrait.class.php
@@ -23,7 +23,7 @@
     }
 
     /**
-     * Get all contents from the database as an array of Contents object
+     * Get all contents related to a user from the database as an array of Contents object
      * TODO Either change the code implementation with Join or Table implementation
      * @param String : $users The username of the user to filter with
      * @return Contents Array : $contents All contents in the database By a specific user
@@ -34,6 +34,21 @@
       $query_users = $qb->select('id')->from(DB_PREFIX.'users')->where('username='.$users);
       $user = $qb->query($query_users);
       $query = $qb->select('*')->from($this->getTable())->where('users_id='.$user.id);
+      return $qb->query($query, get_class($this));
+    }
+
+    /**
+     * Get all contents related to a category from the database as an array of Contents object
+     * TODO Either change the code implementation with Join or Table implementation
+     * @param String : $categories The category name of the category to filter with
+     * @return Contents Array : $contents All contents in the database By a specific user
+     */
+    public function getAllByCategories($categories)
+    {
+      $qb = new QueryBuilder();
+      $query_categories = $qb->select('categories_id')->from(DB_PREFIX.'categories')->where('name='.$categories);
+      $category = $qb->query($query_categories);
+      $query = $qb->select('*')->from($this->getTable())->where('categories_id='.$category.id);
       return $qb->query($query, get_class($this));
     }
 
