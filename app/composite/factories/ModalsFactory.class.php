f56943dc35b15ac16c4a712aeecfc69c2e0af8b7
Thomas DUDOUX <thomas.dudoux@gmail.com>
2017-05-11 18:59:06 +0200

    Started User Logic

--- app/composite/factories/ModalsFactory.class.php
+++ app/composite/factories/ModalsFactory.class.php
@@ -23,19 +23,19 @@
           "submit"=>"Register"
           ],
         "struct" => [
-          "firstname"=>["label"=>"You're firstname : ", "type"=>"text", "placeholder"=>"Firstname", "required"=>"required", "msgerror"=>""],
+          "firstname"=>["label"=>"Your firstname : ", "type"=>"text", "placeholder"=>"Firstname", "required"=>"required", "msgerror"=>""],
 
-          "lastname"=>["label"=>"You're lastname : ", "type"=>"text", "placeholder"=>"Lastname", "required"=>"required", "msgerror"=>""],
+          "lastname"=>["label"=>"Your lastname : ", "type"=>"text", "placeholder"=>"Lastname", "required"=>"required", "msgerror"=>""],
 
-          "username"=>[ "label"=>"You're username : ", "type"=>"text", "placeholder"=>"Username", "required"=>"required", "msgerror"=>"" ],
+          "username"=>[ "label"=>"Your username : ", "type"=>"text", "placeholder"=>"Username", "required"=>"required", "msgerror"=>"" ],
 
-          "user_email"=>[ "label"=>"You're Email : ", "type"=>"email", "placeholder"=>"Email", "required"=>"required", "errors_msg"=>"" ],
+          "user_email"=>[ "label"=>"Your Email : ", "type"=>"email", "placeholder"=>"Email", "required"=>"required", "errors_msg"=>"" ],
 
-          "user_pwd"=>[ "label"=>"You're Password : ", "type"=>"password", "placeholder"=>"Password", "required"=>"required", "errors_msg"=>"" ],
+          "user_pwd"=>[ "label"=>"Your Password : ", "type"=>"password", "placeholder"=>"Password", "required"=>"required", "errors_msg"=>"" ],
 
-          "user_pwd2"=>[ "label"=>"Confirm you're password : ", "type"=>"password", "placeholder"=>"Confirm password", "required"=>"required", "errors_msg"=>"" ],
+          "user_pwd2"=>[ "label"=>"Confirm Your password : ", "type"=>"password", "placeholder"=>"Confirm password", "required"=>"required", "errors_msg"=>"" ],
 
-          "user_img"=>[ "label"=>"Choose you're avatar : ", "type"=>"file", "required"=>"required", "msgerror"=>"" ],
+          "user_img"=>[ "label"=>"Choose Your avatar : ", "type"=>"file", "required"=>"required", "msgerror"=>"" ],
         ]
       ];
     }
@@ -52,9 +52,9 @@
           "submit"=>"Login"
           ],
         "struct" => [
-          "username"=>[ "label"=>"You're username : ", "type"=>"text", "placeholder"=>"Username", "required"=>"required", "msgerror"=>"" ],
+          "username"=>[ "label"=>"Your username : ", "type"=>"text", "placeholder"=>"Username", "required"=>"required", "msgerror"=>"" ],
 
-          "user_pwd"=>[ "label"=>"You're Password : ", "type"=>"password", "placeholder"=>"Password", "required"=>"required", "errors_msg"=>"" ],
+          "user_pwd"=>[ "label"=>"Your Password : ", "type"=>"password", "placeholder"=>"Password", "required"=>"required", "errors_msg"=>"" ],
         ]
       ];
     }
@@ -73,6 +73,36 @@
           "username"=>[ "i"=>["class"=>"fa fa-user", "aria-hidden"=>"true"], /*"label"=>"Your username : ",*/ "type"=>"text", "placeholder"=>"Username", "required"=>"required", "id"=>"username" ],
 
           "password"=>[ "i"=>["class"=>"fa fa-key", "aria-hidden"=>"true"], /*"label"=>"Your Password : ",*/ "type"=>"password", "placeholder"=>"Password", "required"=>"required", "id"=>"password" ],
+        ]
+      ];
+    }
+
+    public static function adminAddUserForm($user)
+    {
+      return [
+        "options" => [
+          "method"=>"POST",
+          "action"=>"admin/doUpdate/".$user,
+          "id"=>"admin_register_user",
+          "enctype"=>"multipart/form-data",
+          "submit"=>"Add User"
+          ],
+        "struct" => [
+          "firstname"=>["label"=>"The firstname : ", "type"=>"text", "placeholder"=>"Firstname", "required"=>"required", "msgerror"=>""],
+
+          "lastname"=>["label"=>"The lastname : ", "type"=>"text", "placeholder"=>"Lastname", "required"=>"required", "msgerror"=>""],
+
+          "username"=>[ "label"=>"The username : ", "type"=>"text", "placeholder"=>"Username", "required"=>"required", "msgerror"=>"" ],
+
+          "user_email"=>[ "label"=>"The Email : ", "type"=>"email", "placeholder"=>"Email", "required"=>"required", "errors_msg"=>"" ],
+
+          "user_pwd"=>[ "label"=>"The Password : ", "type"=>"password", "placeholder"=>"Password", "required"=>"required", "errors_msg"=>"" ],
+
+          "user_pwd2"=>[ "label"=>"Confirm The password : ", "type"=>"password", "placeholder"=>"Confirm password", "required"=>"required", "errors_msg"=>"" ],
+
+          "user_rights"=>["label"=>"Rights of the user", "type"=>"checkbox", "required"=>"required", "value"=>["User"=>true, "Writer"=>false, "Admin"=>false]],
+
+          "user_img"=>[ "label"=>"Choose The avatar : ", "type"=>"file", "required"=>"required", "msgerror"=>"" ],
         ]
       ];
     }
