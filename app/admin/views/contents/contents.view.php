c16fdff190722202a6052f2fd145a5a32d2cd98b
gurnav <singh.g@outlook.fr>
2017-07-26 02:15:57 +0200

    added static paging on contents back and style for comment signal

--- app/admin/views/contents/contents.view.php
+++ app/admin/views/contents/contents.view.php
@@ -50,6 +50,17 @@
             </tbody>
         </table>
 
+        <!-- PAGINATION -->
+        <div class="pagination">
+            <a href="#">&laquo;</a><!-- Previous -->
+            <a class="active"href="#">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+            <a href="#">&raquo;</a><!-- Next -->
+        </div>
+
         <button class="view_more"><a href="<?php echo BASE_URL.'admin'; ?>">return</a></button>
 
     </div>
